5f5c28c9197e9db6e1100197ba8fa605dfa1f07a
Vadim <kozlov67b@gmail.com>
2025-05-03 12:26:12 +0300

    hw_42:Добавление URL-маршрутов и настройка админ-панели eview.

--- barbershop/core/urls.py
+++ barbershop/core/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path('thanks/', views.thanks, name='thanks'),
     path('orders/', views.orders_list, name='orders_list'),
     path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
+    # Новые маршруты для отзывов
+    path('reviews/create/', views.create_review, name='create_review'),
+    path('reviews/thanks/', views.review_thanks, name='review_thanks'),
+    path('api/master-info/', views.get_master_info, name='get_master_info'),
 
     # API автозаполнения
     path('api/autocomplete/client-name/', views.client_name_autocomplete, name='client_name_autocomplete'),
